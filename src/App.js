ac32402f1c9f48f5a841399caccfe558e7153dde
Kris220019 <pochtovaya.kristina@mail.ru>
2021-01-11 07:43:08 +0300

    create weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import  Info from './components/info';
+import Form from './components/form';
+import Weather from './components/weather';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const API_KEY = '6ddc9bf47cfee7d742eede0564af0677';
+
+class App extends React.Component {
+
+  state = {
+    temp: undefined,
+    city: undefined,
+    country: undefined,
+    pressure: undefined,
+    sunset: undefined,
+    error: undefined
+  }
+
+   refreshPage = ()=>{
+    window.location.reload();
+ }
+
+  gettingWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+
+    if (city) {
+      try {
+      const api_url = await fetch( `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+      const  data = await api_url.json();
+
+      let sunset = data.sys.sunset;
+      let date = new Date();
+      date.setTime(sunset);
+      let sunset_date = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+      this.setState({
+        temp: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        pressure: data.main.pressure,
+        sunset: sunset_date,
+        error: undefined
+        });
+      
+    } catch (error) {
+      this.setState((error) => ({error}));
+    }
+    }
+  }
+
+render() {
+  if (this.state.error) {
+    return (
+      <div className="wrapper">
+      <div className="main"> 
+      <div className="container">
+        <div className="row">
+            <div className="col-sm-5 info">
+            
+            </div>
+            <div className="col-sm-7 form">
+            <div>
+    
+      <button onClick={this.refreshPage}>Refresh</button>
     </div>
-  );
+    </div>
+            </div>
+        </div>
+      </div>
+      </div>
+    )
+  } else {
+  return (
+    <div className="wrapper">
+      <div className="main"> 
+      <div className="container">
+        <div className="row">
+            <div className="col-sm-5 info">
+            <Info />
+            </div>
+            <div className="col-sm-7 form">
+              <Form weatherMethod={this.gettingWeather}/>
+              <Weather 
+              temp={this.state.temp}
+              city={this.state.city}
+              country={this.state.country}
+              pressure={this.state.pressure}
+              sunset={this.state.sunset}
+              error={this.state.error}
+              />
+            </div>
+        </div>
+      </div>
+      </div>
+    </div>
+  )
+}
+}
 }
 
 export default App;
